f23bae8ca4aca8f52259fe67882a4b75257f6368
Ong Zhen Ming <ongzhenming@gmail.com>
2018-04-21 14:29:18 +0800

    able to display cell number on click and tidy up design of the board

--- js/ms.js
+++ js/ms.js
@@ -3,14 +3,20 @@ var vueApp = new Vue({
     data: {
         test: "vue work!",
         cellMatrix: generateCellMatrix(),
-    }
+    },
+    methods: {
+        cellClicked: function(x, y, event) {
+            console.log(x + ', ' + y);
+            event.target.textContent = this.cellMatrix[x][y];
+        }
+    },
 });
 
 function generateCellMatrix() {
     return [
-    [1,2,3,4],
-    [4,2,8,9],
-    [5,2,1,6],
-    [1,6,0,3],
+        [1,2,3,4],
+        [4,2,8,9],
+        [5,2,1,6],
+        [1,6,0,3],
     ];
 }
