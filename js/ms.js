6659593fb3da794e873737c24bfa0a772f10c2b7
Ong Zhen Ming <ongzhenming@gmail.com>
2018-04-21 15:10:02 +0800

    able to generate random mines on board

--- js/ms.js
+++ js/ms.js
@@ -1,8 +1,12 @@
+const NUMBER_OF_ROWS = 6;
+const NUMBER_OF_COLUMNS = 6;
+const NUMBER_OF_MINES = 6;
+
 var vueApp = new Vue({
     el: '#vue-app',
     data: {
         test: "vue work!",
-        cellMatrix: generateCellMatrix(),
+        cellMatrix: generateCellMatrix(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, NUMBER_OF_MINES),
     },
     methods: {
         cellClicked: function(x, y, event) {
@@ -12,11 +16,38 @@ var vueApp = new Vue({
     },
 });
 
-function generateCellMatrix() {
-    return [
-        [1,2,3,4],
-        [4,2,8,9],
-        [5,2,1,6],
-        [1,6,0,3],
-    ];
+function generateCellMatrix(numberOfRows, numberOfColumns, numberOfMines) {
+    var numberOfCells = numberOfRows * numberOfColumns;
+    var minesSet = getMines(numberOfMines, numberOfCells);
+    var matrix = [];
+    for (var i = 0; i < numberOfRows; i++) {
+        var row = [];
+        for (var j = 0; j < numberOfColumns; j++) {
+            var cellIndex = i * numberOfColumns + j
+            if (minesSet.has(cellIndex)) {
+                row.push('X');
+            } else {
+                row.push('O');
+            }
+        }
+        matrix.push(row);
+    }
+    return matrix;
+}
+
+function getMines(numberOfMines, numberOfCells) {
+    // returns a set of the cell index of the bombs
+    var minesSet = new Set();
+    while (minesSet.size < numberOfMines) {
+        minesSet.add(getRandomInt(0, numberOfCells));
+    }
+    console.log(minesSet); 
+    return minesSet;
+}
+
+function getRandomInt(min, max) {
+    // The minimum is inclusive and the maximum is exclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
 }
